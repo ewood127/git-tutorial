6d9e2790b211243d01d4dc9b7362b4480506f7b1
Erin Wood <ewood8@unl.edu>
2018-10-19 11:55:14 -0500

    made a histogram

--- fish-analysis.R
+++ fish-analysis.R
@@ -8,4 +8,10 @@ fish <- read_csv("https://raw.githubusercontent.com/ewood127/git-tutorial/master
 fish %>%
   mutate(length_cat= if_else(length >= 300, "big", "small"))-> fish2
 
-         
+fish2 %>% 
+  ggplot()+
+  geom_histogram(aes(scalelength, fill= length_cat))+
+  facet_wrap(~length_cat, nrow= 1)+
+         theme_pres()
+
+
